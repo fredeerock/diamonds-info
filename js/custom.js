8fc39bc3e777772c32a2f32cad28dc457b377eb5
Derick Ostrenko <dostrenko@gmail.com>
2016-03-07 23:21:47 -0600

    done

--- js/custom.js
+++ js/custom.js
@@ -26,3 +26,31 @@ $(function() {
     }
   });
 });
+
+
+ function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'dump.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+      (function init() {
+         loadJSON(function(response) {
+          // Parse JSON string into object
+            var actual_JSON = JSON.parse(response);
+            var genpoem = actual_JSON[0].generatedPoem.join("\n\n");
+            console.log(actual_JSON);
+            document.getElementById("txt").innerHTML = genpoem;
+
+         });
+        })();
+
+
